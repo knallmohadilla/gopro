04c8e8578830d6bb2896bb702138a4a61c6cca61
knallmohadilla <danny.kratt@accenture.com>
2024-05-24 18:25:12 +0200

    modify installl

--- task4/install.sh
+++ task4/install.sh
@@ -1,7 +1,7 @@
 set -x
 eval $(minikube docker-env)
-kubectl delete deployments --all
-kubectl delete services --all
+#kubectl delete deployments --all
+#kubectl delete services --all
 
 (cd backend ; sh dockerbuild.sh)
 (cd frontend ; sh dockerbuild.sh)
