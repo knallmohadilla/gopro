9f056eae7e584e7e24fcf9665eee2e0ff6537071
knallmohadilla <danny.kratt@accenture.com>
2024-05-27 15:28:02 +0200

    grpc

--- task4/install.sh
+++ task4/install.sh
@@ -1,11 +1,11 @@
 set -x
 eval $(minikube docker-env)
-#kubectl delete deployments --all
-#kubectl delete services --all
+kubectl delete deployments --all
+kubectl delete services --all
 
-(cd backend ; sh dockerbuild.sh)
-(cd frontend ; sh dockerbuild.sh)
-(cd nats ; sh dockerbuild.sh)
+#(cd backend ; sh dockerbuild.sh)
+#(cd frontend ; sh dockerbuild.sh)
+#(cd nats ; sh dockerbuild.sh)
 
 kubectl apply -f natsapp.yaml
 
