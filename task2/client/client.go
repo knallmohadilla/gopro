50c923f34a51fd19732f121d0c0dad660664ff0b
knallmohadilla <danny.kratt@accenture.com>
2024-05-27 17:49:24 +0200

    serverclient

---
+++ task2/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/knallmohadilla/gopro/task2/rps"
+)
+
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGameServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.CreateGame(ctx, &pb.GameRequest{Game: &pb.Game{Id: "1", Name: "game1"}})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetGame())
+}
--- task2/server/server.go
+++ task2/server/server.go
@@ -1,4 +1,4 @@
-package rps
+package main
 
 import (
 	"context"
@@ -15,15 +15,15 @@ var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-// server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedGameServiceServer
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+
+
+func (s *server) CreateGame(ctx context.Context, in *pb.GameRequest) (*pb.GameResponse, error) {
+	log.Printf("Received: %v", in.GetGame())
+	return &pb.GameResponse{Game: &pb.Game{Id: "1", Name: "game1"}}, nil
 }
 
 func main() {
@@ -32,8 +32,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterGameServiceServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
